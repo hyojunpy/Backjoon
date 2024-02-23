98b97f52d2d8b0c7936676f803724530e532a025
Hyojun Pyeon <vusgywns5946@gmail.com>
2024-02-23 21:56:12 +0900

    0223

---
+++ Solve(Baekjoon)_1123/src/Solve0223.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+import java.io.*; 
+
+public class Solve0223 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("비와이");
+	}
+
+}
