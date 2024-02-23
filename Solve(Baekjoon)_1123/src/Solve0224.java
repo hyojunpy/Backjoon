8579162756d4c097ea9fffa29cd5e881b875a563
Hyojun Pyeon <vusgywns5946@gmail.com>
2024-02-24 06:36:32 +0900

    0224

---
+++ Solve(Baekjoon)_1123/src/Solve0224.java
@@ -0,0 +1,14 @@
+//15680 연세대학교
+import java.util.*; 
+import java.io.*; 
+
+public class Solve0224{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		
+		int n = Integer.parseInt(br.readLine()); 
+		
+		if(n == 1) System.out.print("Leading the Way to the Future"); 
+		else System.out.print("YONSEI"); 
+	}
+}
