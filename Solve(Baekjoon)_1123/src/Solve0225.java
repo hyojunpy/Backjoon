619263ba3824cc92921e89d5a7a051218ba2a21c
Hyojun Pyeon <vusgywns5946@gmail.com>
2024-02-25 23:42:07 +0900

    0225

---
+++ Solve(Baekjoon)_1123/src/Solve0225.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+
+
+public class Solve0225 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine()); 
+		
+		System.out.print((int)Math.pow(2, n));
+		
+	}
+
+}
