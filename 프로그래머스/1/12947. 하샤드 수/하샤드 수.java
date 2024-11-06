c3cd82519fa050b99a7c00a4bf70b7e0170c394e
편효준 <118783654+hyojunpy@users.noreply.github.com>
2024-11-06 09:42:09 +0900

    [level 1] Title: 하샤드 수, Time: 8.16 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
         String s = "" + x; 
-        int a = 0;
-        for(int i = 0; i < s.length(); i++) { 
-            a += s.charAt(i) - '0';
+        int a = 0; 
+        for(int i = 0; i < s.length(); i++) {
+            a += s.charAt(i) - '0';  
         }
+        if(x % a != 0) return false;
         
-        return x % a == 0;
+        return true;
     }
 }
