533aecbfeb950d3a3609049febcef91995946ab3
편효준 <118783654+hyojunpy@users.noreply.github.com>
2024-11-12 09:23:48 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -2,22 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        int[] answer = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int result = 0; 
-        
-        
-        for(int i = 0; i < answer.length; i++) {
-            boolean torf = false; 
-            for(int j = 0; j < numbers.length; j++) {
-                if( answer[i] == numbers[j]) {
-                    torf = true; 
-                } 
-            }
-            if(!torf) {
-                result += answer[i];            
-            }
+        int result = 45; 
+        for( int i : numbers) {
+            result -= i; 
         }
-        
         
         return result;
     }
