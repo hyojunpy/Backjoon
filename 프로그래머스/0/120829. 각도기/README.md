63576d99a8b8de8ca2dd7cec8e203103c4b82728
편효준 <118783654+hyojunpy@users.noreply.github.com>
2025-01-03 09:55:41 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/README.md
+++ 프로그래머스/0/120829. 각도기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 각도기 - 120829 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120829) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120829?language=java) 
 
 ### 성능 요약
 
-메모리: 73.3 MB, 시간: 0.03 ms
+메모리: 75.4 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 18일 09:53:36
+2025년 01월 03일 09:55:37
 
 ### 문제 설명
 
--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -2,11 +2,7 @@ class Solution {
     public int solution(int angle) {
         int answer = 0;
         
-        switch(angle) { 
-        case 90 -> answer = 2; 
-                case 180 -> answer = 4; 
-                default -> answer = angle < 90 ? 1 : 3;
-        }
+        answer = (angle < 90) ? 1 : (angle == 90 ) ? 2 : (angle < 180) ? 3 : (angle == 180) ? 4 : 0; 
         
         return answer;
     }
