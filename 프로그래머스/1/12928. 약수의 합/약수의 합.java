ca626d581ca0d1f300a7b5754c5e61b14ee358ed
편효준 <118783654+hyojunpy@users.noreply.github.com>
2025-01-07 23:58:39 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -2,13 +2,12 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        if(0 <=n && n <= 3000) { 
-            for(int i = 1; i <= n / 2; i++) { 
-                if(n % i == 0) {
-                    answer += i; 
-                }
+        for(int i = 1; i <= n/2; i++) {
+            if(n % i == 0) {
+                answer += i;
             }
-        }   
-        return answer + n ;
+        }
+        
+        return answer + n;
     }
 }
