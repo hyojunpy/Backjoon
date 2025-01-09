fd1a65e963811f06315bff57b6d50321f54f9117
편효준 <118783654+hyojunpy@users.noreply.github.com>
2025-01-09 23:07:13 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -2,10 +2,8 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        if ( 0 < n && n <= 1000) { 
-            for(int i = 0; i <= n; i = i + 2) { 
+       for(int i = 2; i <= n; i = i + 2) { 
             answer += i; 
-            }
         }
         return answer;
     }
