17c68ce811f26a744e8e93dcbe2ea985b8aee983
편효준 <118783654+hyojunpy@users.noreply.github.com>
2025-01-10 11:12:15 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -2,14 +2,10 @@ class Solution {
     public double solution(int[] arr) {
         double answer = 0;
         
-        if(1 <= arr.length && arr.length <= 100) {
             for(int i : arr) {
-                if(-10000 <= i && i <= 10000) { 
                     answer += i; 
                 }
-            }
             answer /= arr.length;
-        }
         return answer;
     }
 }
