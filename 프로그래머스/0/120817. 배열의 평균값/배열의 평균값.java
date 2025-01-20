d4e7f082786dd3849a5120f0273b42573bc0fc7c
편효준 <118783654+hyojunpy@users.noreply.github.com>
2025-01-20 09:03:26 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -3,7 +3,7 @@ class Solution {
         double answer = 0;
         
         for(int i = 0; i < numbers.length; i++) {
-            answer += numbers[i]; 
+            answer += numbers[i];
         }
         
         return answer / numbers.length;
