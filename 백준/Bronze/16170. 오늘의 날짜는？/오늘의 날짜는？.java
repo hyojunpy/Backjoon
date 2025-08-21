8f70a5d0ca2a0922a631127be6c073e6be1a4f70
편효준 <118783654+hyojunpy@users.noreply.github.com>
2025-08-21 22:43:07 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 96 ms, Memory: 13164 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.time.LocalDateTime;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        LocalDateTime now = LocalDateTime.now();
+
+        now.minusHours(9);
+        System.out.println(now.getYear());
+        System.out.println(now.getMonthValue());
+        System.out.println(now.getDayOfMonth());
+    }
+}
