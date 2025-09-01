f1ed4f8d0979ccd2e52ff9f88126810f8534f776
편효준 <118783654+hyojunpy@users.noreply.github.com>
2025-09-01 17:30:57 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 632 ms, Memory: 125544 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        boolean[] arr = new boolean[2000001];
+
+        for(int i = 0; i < N; i++){
+            arr[Integer.parseInt(br.readLine()) + 1000000] = true;
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]) {
+                sb.append(i - 1000000).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
