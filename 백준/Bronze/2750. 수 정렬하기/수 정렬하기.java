337458bde7b51b9b9794b975663ba474efdb800a
편효준 <118783654+hyojunpy@users.noreply.github.com>
2025-09-01 15:46:55 +0900

    [Bronze II] Title: 수 정렬하기, Time: 72 ms, Memory: 11888 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+
+        boolean[] arr= new boolean[2001];
+
+        for(int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine()) + 1000] = true;
+        }
+
+        for(int i = 0; i < 2001; i++) {
+            if(arr[i]){
+                sb.append(i - 1000).append('\n');
+            }
+        }
+        System.out.println(sb);
+
+    }
+}
