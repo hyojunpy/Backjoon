c40a89fb625e1550b1240b198d63e9a2ccdc0c6b
편효준 <118783654+hyojunpy@users.noreply.github.com>
2025-11-13 17:21:01 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 390 ms, Memory: 146,308 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.java
@@ -3,44 +3,30 @@ import java.io.*;
 
 class Solution
 {
-	public static void main(String args[]) throws NumberFormatException, IOException
-	{
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder(); 
+	public static void main(String args[]) throws NumberFormatException, IOException{
+        StringTokenizer st; 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        int TC = Integer.parseInt(br.readLine()); 
         
-        int T = Integer.parseInt(br.readLine());
-        
-        for(int tc = 1; tc <= T; tc++){
-            int n = Integer.parseInt(br.readLine()); 
-            int[] arr = new int[n]; 
+        for(int tc = 1; tc <= TC; tc++) {
+        	int N = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine()); 
+            long arr[] = new long[N]; 
             
-            StringTokenizer st = new StringTokenizer(br.readLine()); 
-            
-            for(int i = 0; st.hasMoreTokens(); i++) {
-                arr[i] = Integer.parseInt(st.nextToken()); 
+            for(int i = 0; i < N; i++) {
+            	arr[i] = Integer.parseInt(st.nextToken()); 
             }
             
-			int len = arr.length; 
-
-            long max = Long.MIN_VALUE; 
-            int num = 0;
-            long cost = 0L; 
-            long result = 0L; 
-            
-            for(int i = len - 1; i >= 0; i--) {
-            	if(arr[i] > max) {
-                	result += (max * num - cost);
-                    max = arr[i]; 
-                    cost = 0; 
-                    num = 0; 
-                } else{
-                    cost += arr[i]; 
-                    num++; 
+            long current = arr[N -1];  
+            long max = 0;
+            for(int i = N -1; i >= 0; i--) {
+            	if(current < arr[i]) {
+                	current = arr[i];
+                }else{
+                	    max += current -arr[i];
                 }
             }
-            result += (max * num - cost); 
-            sb.append("#" + tc + " " + result + "\n"); 
+			System.out.println("#" + tc +  " " + max); 
         }
-        System.out. println(sb); 
     }
 }
