334d599f5d9e51bed29e72e5ff34f9d1f46134af
편효준 <118783654+hyojunpy@users.noreply.github.com>
2025-11-20 17:01:38 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 12324 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,25 +1,28 @@
 import java.awt.*;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     static int n, m;
     static int[][] arr;
-    static boolean[][] vis;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static boolean[][] visited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        arr = new int[n][m];
-        vis = new boolean[n][m];
 
-        for (int i = 0; i < n; i++) {
+        arr = new int[n][m];
+        visited = new boolean[n][m];
+
+        for(int i = 0; i < n; i++){
             String line = br.readLine();
-            for (int j = 0; j < m; j++) {
+            for(int j = 0; j < m; j++){
                 arr[i][j] = line.charAt(j) - '0';
             }
         }
@@ -29,29 +32,31 @@ public class Main {
         System.out.println(arr[n - 1][m - 1]);
     }
 
-    static void bfs(int x, int y) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(x, y));
-        vis[x][y] = true;
+    static void bfs(int x, int y){
+        Queue<Point> q = new java.util.LinkedList<>();
+        q.offer(new Point(x, y));
+        visited[x][y] = true;
 
-        while (!queue.isEmpty()) {
-            Point p = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int nextX = p.x + dx[i];
-                int nextY = p.y + dy[i];
+        while(!q.isEmpty()){
+            Point p = q.poll();
+            for(int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
 
-                if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= m)
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m ){
                     continue;
-                if (arr[nextX][nextY] == 0)
+                }
+                if(arr[nx][ny] == 0) {
                     continue;
-                if (vis[nextX][nextY])
+                }
+                if(visited[nx][ny]) {
                     continue;
-                queue.offer(new Point(nextX, nextY));
-                vis[nextX][nextY] = true;
+                }
 
-                arr[nextX][nextY] = arr[p.x][p.y] + 1;
+                q.offer(new Point(nx, ny));
+                visited[nx][ny] = true;
+                arr[nx][ny] = arr[p.x][p.y] + 1;
             }
         }
     }
-
 }
